9623b4eec7861c2322a0f0bd9d6593fdd7a5d867
lbjyang001@gmail.com <yangfan6262008@gmail.com>
2023-08-28 12:26:13 +0800

    change owenr

--- HelloVitalik.js
+++ HelloVitalik.js
@@ -1,7 +1,7 @@
 import {ethers} from "ethers";
 const provider = ethers.getDefaultProvider();
 const main = async() => {
-    const balance = await provider.getBalance('Vitalik.eth');
-    console.log(`ETH Balance of vitalik: ${ethers.formatEther(balance)} ETH`);
+    const balance = await provider.getBalance('lbjyang.eth');
+    console.log(`ETH Balance of lbjyang: ${ethers.formatEther(balance)} ETH`);
 }
 main()
