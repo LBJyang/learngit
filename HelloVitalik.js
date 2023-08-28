f9918b91988cf8cbc4296af9a24726c31e4b5b53
lbjyang001@gmail.com <yangfan6262008@gmail.com>
2023-08-28 12:32:19 +0800

    add vitalik

--- HelloVitalik.js
+++ HelloVitalik.js
@@ -2,6 +2,9 @@ import {ethers} from "ethers";
 const provider = ethers.getDefaultProvider();
 const main = async() => {
     const balance = await provider.getBalance('lbjyang.eth');
+    const balanceOfVitalik = await provider.getBalance('vitalik.eth')
     console.log(`ETH Balance of lbjyang: ${ethers.formatEther(balance)} ETH`);
+    console.log(`ETH Balance of Vitalik：${ethers.formatEther(balanceOfVitalik)}ETH`)
+    
 }
 main()
