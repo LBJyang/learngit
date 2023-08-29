ff1bdab53a32d5a7b3a5eb00091749d11ce4ced4
lbjyang001@gmail.com <yangfan6262008@gmail.com>
2023-08-29 15:05:08 +0800

    add mjs

--- HelloVitalik.mjs
+++ HelloVitalik.mjs
@@ -1,7 +1,7 @@
 import {ethers} from "ethers";
-const provider = ethers.getDefaultProvider();
+const provider = ethers.getDefaultProvider()
 const main = async() => {
-    const balance = await provider.getBalance('lbjyang.eth');
-    console.log(`ETH Balance of lbjyang: ${ethers.formatEther(balance)} ETH`);
+    const balance = await provider.getBalance('0xf102ca9c4d65b0B94412bf1809833ADb1F87DBCF');
+    console.log(`My ETH Balance: ${ethers.utils.formatEther(balance)} ETH`);
 }
 main()
