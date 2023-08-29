2d441ece302eeebd953950106b706853ce8e1beb
lbjyang001@gmail.com <yangfan6262008@gmail.com>
2023-08-29 14:55:03 +0800

    delete js

--- HelloVitalik.js
+++
@@ -1,7 +0,0 @@
-import {ethers} from "ethers";
-const provider = ethers.getDefaultProvider();
-const main = async() => {
-    const balance = await provider.getBalance('lbjyang.eth');
-    console.log(`ETH Balance of lbjyang: ${ethers.formatEther(balance)} ETH`);
-}
-main()
